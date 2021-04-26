6748afd308d427a10382882a5cdcfca86f24e8a9
Jakub <jflaska@baosystems.com>
2021-04-26 10:18:06 -0400

    working

--- src/modules/test/sendData/mockSendData.service.ts
+++ src/modules/test/sendData/mockSendData.service.ts
@@ -1,20 +1,21 @@
 
 let registeredMocks = {};
 
-export function registerSendMock(method:string, url:string, response: object, test: (object)=>void){
-    registeredMocks[url] = {test, method, response};
+export function registerSendMock(method:string, url:string, response: object):Promise<any>{
+    return new Promise((resolve)=>{
+        registeredMocks[url] = {method, response, resolve};
+    });
 }
 
 function isMocked(url:string):boolean{
     return registeredMocks[url];
 }
 
-export function mockSendData(method:string, url:string, data:any):Promise<object>{
+export function mockSendData(mtd:string, url:string, data:any):Promise<object>{
     url = url.replace(/^http.+\/api/,'');
     if (!isMocked(url)) throw new Error(`sending data to server in test env: ${url}`);
-    let mock = registeredMocks[url];
-    if (mock.test) mock.test(data);
-    // @ts-ignore
-    expect(method).toBe(mock.method);
-    return Promise.resolve(mock.response);
+    let {response, method, resolve} = registeredMocks[url];
+    resolve(data);
+    if (mtd!==method) throw new Error(`Send data HTTP method not matching ${mtd} vs ${method}`);
+    return Promise.resolve(response);
 }
